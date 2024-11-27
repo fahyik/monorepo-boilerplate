92d6266c201d460a06c8895364bf145730679fcb
fahyik <fahyik@gmail.com>
2024-11-28 00:40:33 +0100

    chore: reset colors

--- apps/web/src/app/(home)/page.tsx
+++ apps/web/src/app/(home)/page.tsx
@@ -41,8 +41,8 @@ function ModeToggle() {
 
 export default function Home() {
   return (
-    <div className="flex flex-1 h-screen items-center justify-center bg-gray-100 dark:bg-gray-700">
-      <div className="w-full max-w-screen-md p-8 bg-white dark:bg-gray-500 shadow-lg rounded-lg">
+    <div className="flex flex-1 h-screen items-center justify-center bg-background text-foreground">
+      <div className="w-full max-w-screen-md p-8 shadow-lg rounded-lg">
         <h1 className="text-4xl font-bold text-center mb-8">HOME PAGE</h1>
         <ModeToggle />
       </div>
